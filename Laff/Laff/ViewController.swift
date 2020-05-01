be02aa17f47d4a4aa4e4ef7be0354c9046a72bc1
nolant108 <43392716+nolant108@users.noreply.github.com>
2020-05-01 09:24:16 -0700

    Main Update

---
+++ Laff/Laff/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Laff
+//
+//  Created by Nolan Trapp on 4/30/20.
+//  Copyright © 2020 RedTechRetail. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+    }
+
+
+}
+
